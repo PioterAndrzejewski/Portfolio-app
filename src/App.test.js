7dda5c7cd60827192f70268f36b8548803cac4c3
PioterAndrzejewski <109315248+PioterAndrzejewski@users.noreply.github.com>
2023-01-13 12:22:10 +0100

    create basic cases to test the form

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,87 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {
+	render,
+	screen,
+	fireEvent,
+	act,
+	waitForElement,
+} from "@testing-library/react";
+import ContactMeSection from "./components/ContactMeSection";
+import { AlertProvider } from "./context/alertContext";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Contact-me form", () => {
+	test("Submission is disabled if there is no name provided", () => {
+		const jestSubmit = jest.fn();
+
+		render(
+			<AlertProvider>
+				<ContactMeSection onSubmit={jestSubmit} />
+			</AlertProvider>
+		);
+
+		const emailInput = screen.getByLabelText(/Email Address/);
+		fireEvent.change(emailInput, { target: { value: "example@example.com" } });
+
+		const messageInput = screen.getByLabelText(/Your message/);
+		fireEvent.change(messageInput, {
+			target: {
+				value: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+			},
+		});
+
+		const submitButton = screen.getByRole("button");
+
+		act(() => {
+			fireEvent.click(submitButton);
+		});
+
+		expect(jestSubmit).not.toHaveBeenCalled();
+		expect(submitButton).toHaveAttribute("disabled");
+	});
+
+	test("Submission is disabled if there is no email provided", () => {
+		const jestSubmit = jest.fn();
+		render(
+			<AlertProvider>
+				<ContactMeSection onSubmit={jestSubmit} />
+			</AlertProvider>
+		);
+		const nameInput = screen.getByLabelText(/Name/);
+		fireEvent.change(nameInput, { target: { value: "Andrzej" } });
+
+		const messageInput = screen.getByLabelText(/Your message/);
+		fireEvent.change(messageInput, {
+			target: {
+				value: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+			},
+		});
+
+		const submitButton = screen.getByRole("button");
+		fireEvent.click(submitButton);
+
+		expect(jestSubmit).not.toHaveBeenCalled();
+		expect(submitButton).toHaveAttribute("disabled");
+	});
+
+	test("Submission is disabled if there is no comment provided", () => {
+		const jestSubmit = jest.fn();
+		render(
+			<AlertProvider>
+				<ContactMeSection onSubmit={jestSubmit} />
+			</AlertProvider>
+		);
+		const nameInput = screen.getByLabelText(/Name/);
+		fireEvent.change(nameInput, { target: { value: "Andrzej" } });
+
+		const emailInput = screen.getByLabelText(/Email Address/);
+		fireEvent.change(emailInput, { target: { value: "example@example.com" } });
+
+		const submitButton = screen.getByRole("button");
+		fireEvent.click(submitButton);
+
+		expect(jestSubmit).not.toHaveBeenCalled();
+		expect(submitButton).toHaveAttribute("disabled");
+	});
+
+	// TODO: submit case
+	// TODO: solve act() issue
 });
